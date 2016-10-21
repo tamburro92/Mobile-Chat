2a7be78e4d5931159bfbfe84846dff48813317c6
artur <artur@172.19.33.47>
2016-10-21 11:31:59 +0200

    refresh

---
+++ src/client/Client.java
@@ -0,0 +1,10 @@
+package client;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class Client {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+	}
+	
+}
